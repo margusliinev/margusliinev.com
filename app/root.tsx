fb14ca1a1e86253fe6fbcf2e725b5e087ad85f39
Margus Liinev <margus.liinev@gmail.com>
2025-09-02 22:57:51 +0300

    create separate styles folder

--- app/root.tsx
+++ app/root.tsx
@@ -1,6 +1,6 @@
 import type { Route } from './+types/root';
 import { isRouteErrorResponse, Links, Meta, Outlet, Scripts, ScrollRestoration } from 'react-router';
-import './app.css';
+import './styles/index.css';
 
 export const links: Route.LinksFunction = () => [
     { rel: 'icon', href: '/favicon.ico' },
