525176904b5f83a3e63e6386c10a918ab1db25ec
Margus Liinev <margus.liinev@gmail.com>
2025-09-06 16:27:31 +0300

    update how devtools are configured

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -50,7 +50,7 @@ function Root() {
     return (
         <Document>
             <Outlet />
-            {import.meta.env.MODE !== 'production' && <TanStackRouterDevtools position='bottom-right' />}
+            {import.meta.env.DEV && <TanStackRouterDevtools position='bottom-right' />}
         </Document>
     );
 }
