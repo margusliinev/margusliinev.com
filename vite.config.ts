5e7006f54ba6e80f110f048386a8bb4c49086a53
Margus Liinev <margus.liinev@gmail.com>
2025-09-06 22:33:49 +0300

    add vite-ts-config-paths package

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,18 @@
 import { tanstackStart } from '@tanstack/react-start/plugin/vite';
 import { defineConfig } from 'vite';
+import viteTsConfigPaths from 'vite-tsconfig-paths';
 import viteReact from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
     server: { port: 3000 },
     build: { outDir: 'build' },
-    plugins: [tanstackStart(), viteReact(), tailwindcss()],
+    plugins: [
+        viteTsConfigPaths({
+            projects: ['./tsconfig.json'],
+        }),
+        tanstackStart(),
+        viteReact(),
+        tailwindcss(),
+    ],
 });
