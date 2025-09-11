73b30e2252d7148fd465dfc0da2e390309658c9d
Margus Liinev <margus.liinev@gmail.com>
2025-09-11 14:32:34 +0300

    rename icon.svg to logo.svg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import Logo from './icon.svg';
+import Logo from './logo.svg';
 
 export default async function Home() {
     return (
